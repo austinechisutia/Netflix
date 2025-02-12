cccd64a12846dd834f567338887ab2d6fd629edf
austinechisutia <austineakhonya624@gmail.com>
2025-02-12 10:05:18 +0300

    finalized my app landing page

---
+++ main.js
@@ -0,0 +1,22 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+// select tab content item
+function selectItem(e){
+    // add border to cureent tab
+    removeBorder();
+    removeShow()
+    this.classList.add('tab-border');
+    // grab content item from the dom
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    // Add Show class
+    tabContentItem.classList.add('show'); 
+}
+function removeBorder(){
+    tabItems.forEach(item=>item.classList.remove('tab-border'))
+}
+function removeShow(){
+    tabContentItems.forEach(item=>item.classList.remove('show'))
+}
+
+// Listen for tab click
+tabItems.forEach(item=>item.addEventListener('click', selectItem));
